2368c595522973bdceff88c44ff76fbc9897b7c3
Chaitya W <chaityaw@Chaityas-MacBook-Air.local>
2025-08-10 14:02:42 +0530

    card and readme changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,12 @@ import axios from "axios";
 
 const Card = ({ title, imageUrl }) => {
   return (
-    <div className="bg-gray-200 p-7 m-2 flex flex-col justify-center rounded-xl">
+    <div className="bg-gray-200 p-4 m-2 flex flex-col justify-center rounded-xl">
       <div className="">{title}</div>
       <img
         src={imageUrl}
         alt={title}
-        className="w-52 h-36 object-cover rounded-lg mt-2"
+        className="w-full h-36 object-cover rounded-lg mt-2"
       />
     </div>
   );
